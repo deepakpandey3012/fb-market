cc82b84f551f040788f175a6af0c9722a63b2d0b
Dipesh Labh <dipeshlav@gmail.com>
2021-12-01 19:22:31 +0545

    deploy bug fixes

--- src/app/campaign.service.ts
+++ src/app/campaign.service.ts
@@ -2,31 +2,36 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CampaignService {
+  getDefaultCampaignsUrl = 'http://3.135.233.196/api/campaign/';
+  postDefaultCampaignsUrl = 'http://3.135.233.196/api/campaign/';
+  sendCredentialsUrl = 'http://3.135.233.196/api/accountSecrets';
+  constructor(private http: HttpClient) {}
 
-  getDefaultCampaignsUrl = "http://127.0.0.1:8000/campaign/";
-  postDefaultCampaignsUrl = "http://127.0.0.1:8000/campaign/";
-  sendCredentialsUrl = "http://127.0.0.1:8000/campaign/accountSecrets";
-  constructor(private http: HttpClient) {   }
-
-  getDefaultCampaigns(dateRange: string[], preSet){
-    if(dateRange.length!=0 && !preSet){
-      return this.http.get(`${this.getDefaultCampaignsUrl}?time_range={"since":"${dateRange[0]}","until":"${dateRange[1]}"}`);
-    }else if(dateRange.length==0 && preSet){
-      return this.http.get(`${this.getDefaultCampaignsUrl}?date_preset=${preSet}`);
-    }else if(dateRange.length!=0 && preSet){
-      return this.http.get(`${this.getDefaultCampaignsUrl}?date_preset=${preSet}&time_range={"since":"${dateRange[0]}","until":"${dateRange[1]}"}`);
+  getDefaultCampaigns(dateRange: string[], preSet) {
+    if (dateRange.length != 0 && !preSet) {
+      return this.http.get(
+        `${this.getDefaultCampaignsUrl}?time_range={"since":"${dateRange[0]}","until":"${dateRange[1]}"}`
+      );
+    } else if (dateRange.length == 0 && preSet) {
+      return this.http.get(
+        `${this.getDefaultCampaignsUrl}?date_preset=${preSet}`
+      );
+    } else if (dateRange.length != 0 && preSet) {
+      return this.http.get(
+        `${this.getDefaultCampaignsUrl}?date_preset=${preSet}&time_range={"since":"${dateRange[0]}","until":"${dateRange[1]}"}`
+      );
     }
-     return this.http.get(`${this.getDefaultCampaignsUrl}`);
+    return this.http.get(`${this.getDefaultCampaignsUrl}`);
   }
 
-  createCampaign(data){
+  createCampaign(data) {
     return this.http.post(this.postDefaultCampaignsUrl, data);
- }
+  }
 
- sendCredentials(data){
-  return this.http.post(this.sendCredentialsUrl, data);
-}
+  sendCredentials(data) {
+    return this.http.post(this.sendCredentialsUrl, data);
+  }
 }
